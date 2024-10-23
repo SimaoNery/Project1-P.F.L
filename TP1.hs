0bad09d95fcb7c568a202c2f604d8f176d3c6056
Leandro Martins <145551027+leandro-martins-2004@users.noreply.github.com>
2024-10-23 14:22:07 +0100

    Solved function 2 and 4

--- TP1.hs
+++ TP1.hs
@@ -6,41 +6,68 @@
 
 -- Uncomment the some/all of the first three lines to import the modules, do not change the code of these lines.
 
---Project Types---------------------------------------------------------------------------------------------------------------------------------------------
 type City = String
 type Path = [City]
 type Distance = Int
 
 type RoadMap = [(City,City,Distance)]
 
---Project Functions-----------------------------------------------------------------------------------------------------------------------------------------------------------
+--1
 cities :: RoadMap -> [City]
 cities = undefined -- modifiy this line to implement the solution, for each exercise not solved, leave the function definition like this
 
-areAdjacent :: RoadMap -> City -> City -> Bool
-areAdjacent = undefined
+--2
+areAdjacent :: RoadMap -> City -> City -> Bool -- First parameter is the Graph, second and third parameters are the cities names that we will
+                                               -- find out if they are adjacent (have a direct path inbetween them), in this case, if there's a tuple
+                                               -- with both cities. It returns True if they are adjacent, and False if they are not.
 
+areAdjacent _ c1 c2 | c1 == c2 = False -- Returns False if the cities from the function parameters are the same since they will never be adjacent
+
+areAdjacent [] _ _ = False  -- If the RoadMap is empty, returns False, could happen when the RoadMap starts empty 
+                            -- or when no direct path was found inbetween both cities, or one city didn't exist, or both cities are not adjacent
+
+areAdjacent ((rc1, rc2, _):map) c1 c2 -- saves on rc1 and rc2 the cities of the first tuple of the list
+
+        | (((rc1 == c1) && (rc2 == c2)) || ((rc2 == c1) && (rc1 == c2))) = True -- if both cities of the tuple are the same as the cities given in 
+                                                                                -- the function parameters, then a direct path was found and returns True
+
+        | otherwise = areAdjacent map c1 c2 -- if this tuple was not a  direct path inbetween the cities of the function parameters, passes the rest of the
+                                            -- list and does everything again
+
+--3
 distance :: RoadMap -> City -> City -> Maybe Distance
 distance = undefined
 
+--4
 adjacent :: RoadMap -> City -> [(City,Distance)]
-adjacent = undefined
+adjacent [] _ = []
+adjacent ((rc1, rc2, dist):map) c1
+        | rc1 == c1 = (rc2, dist) : adjacent map c1
+        | rc2 == c1 = (rc1, dist) : adjacent map c1
+        | otherwise = adjacent map c1 
 
+--5
 pathDistance :: RoadMap -> Path -> Maybe Distance
 pathDistance = undefined
 
+--6
 rome :: RoadMap -> [City]
-rome = undefined
+rome [] = []
+rome ((c1,c2,_):map) = undefined
 
+--7
 isStronglyConnected :: RoadMap -> Bool
 isStronglyConnected = undefined
 
+--8
 shortestPath :: RoadMap -> City -> City -> [Path]
 shortestPath = undefined
 
+--9
 travelSales :: RoadMap -> Path
 travelSales = undefined
 
+--10
 tspBruteForce :: RoadMap -> Path
 tspBruteForce = undefined -- only for groups of 3 people; groups of 2 people: do not edit this function
 
